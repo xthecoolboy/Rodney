bed94d3ecbcac22043278c2cf8df3b09628caf89
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-14 21:44:25 +0200

    Create swear.js

---
+++ commands/swear.js
@@ -0,0 +1,7 @@
+bot.on("message", message => {
+    //Here you can put banned words and swearwords in the quotation marks "here"
+    const swearWords = ["word", "word", "word", "word", "word", "word", "word", "word"];
+    if( swearWords.some(word => message.content.includes(word)) ) {
+      message.reply("This word is banned by the server owner");
+      
+    }} )
