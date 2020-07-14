2930f9b460e55d8610d7beb963fc428090c9c35d
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-14 21:45:29 +0200

    Delete swear.js

--- commands/swear.js
+++
@@ -1,7 +0,0 @@
-bot.on("message", message => {
-    //Here you can put banned words and swearwords in the quotation marks "here"
-    const swearWords = ["testtt", "word", "word", "word", "word", "word", "word", "word"];
-    if( swearWords.some(word => message.content.includes(word)) ) {
-      message.reply("This word is banned by the server owner");
-      
-    }} )
